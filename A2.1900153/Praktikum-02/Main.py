7e62bdd4a57da8dd70e13b333fc47683b0b18dbe
rikiseptiana <riki.elflash3@gmail.com>
2020-10-03 10:47:24 +0700

    Membuat Main.py

---
+++ A2.1900153/Praktikum-02/Main.py
@@ -0,0 +1,20 @@
+class Hero : # template
+    pass
+
+
+hero1 = Hero() # object
+hero2 = Hero()
+hero3 = Hero();
+
+hero1.name = "Sniper"
+hero1.health = 1000
+
+hero2.name = "Kagura"
+hero2.health = 1500
+
+hero3.name = "Balmond"
+hero3.health = 2500
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
