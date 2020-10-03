6f6f19258cac55a9718e0533241e513447008b9f
SilviFauziaAdzani <silvifauzia4901@gmail.com>
2020-10-03 17:36:28 +0700

    Main.py

--- A2.1900170/Main.py
+++ A2.1900170/Main.py
@@ -10,10 +10,10 @@ hero1.name = "sniper"
 hero1.health= 100
 
 hero2.name = "sven"
-hero2.health= 100
+hero2.health= 200
 
 hero3.name = "ucup"
-hero3.health= 100
+hero3.health= 1000
 
 print(hero1)
 print(hero1.__dict__)
