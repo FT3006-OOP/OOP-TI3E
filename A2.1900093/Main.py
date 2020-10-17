75d818d9758fbe213e3c81665169885d48d1eb31
Jimmi Andreansa <jimi.rgt17@gmail.com>
2020-10-17 17:35:41 +0700

    Menambahkan file Main.py

--- A2.1900093/Main.py
+++ A2.1900093/Main.py
@@ -1,20 +1,16 @@
-class Hero: #template
-    pass
+class Hero: # template
+
+    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
 
 
-hero1 = Hero() #object / instance
-hero2 = Hero()
-hero3 = Hero();
+hero1 = Hero("sniper",100, 10, 4)
+hero2 = Hero("mirana",100, 15, 1)
+hero3 = Hero("ucup",1000, 100, 0)
 
-hero1.name = "sniper"
-hero1.health = 100
-
-hero2.name = "sven"
-hero2.health = 200
-
-hero3.name = "ucup"
-hero3.health = 1000
-
-print(hero1)
 print(hero1.__dict__)
-print(hero1.name)
+print(hero2.__dict__)
+print(hero3.__dict__)
