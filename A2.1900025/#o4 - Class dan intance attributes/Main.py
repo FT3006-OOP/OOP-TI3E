754a06bd4314c44cce235a2827e586b30ef61c76
bagussalam83 <salambagus.bs@gmail.com>
2020-10-19 07:51:08 -0700

    Memahami konseclass Hero: # Template     #class variabel '

--- A2.1900025/#o4 - Class dan intance attributes/Main.py
+++ A2.1900025/#o4 - Class dan intance attributes/Main.py
@@ -3,16 +3,16 @@ class Hero: # Template
     jumlah = 0
 
     def __init__(self, inputName, inputHealth, inputPower, inputArmor):
-        # intance variabel
+        # instance variabel
         self.name = inputName
         self.health = inputHealth
         self.power = inputPower
         self.armor = inputArmor
         Hero.jumlah += 1
-        print ("membuat hero dengan nama" + inputName)
+        print ("membuat Hero dengan nama " + inputName)
 
 
-hero1 = Hero("single",100, 10, 4)
+hero1 = Hero("sniper",100, 10, 4)
 print(Hero.jumlah)
 hero2 = Hero("mirana",100, 15, 1)
 print(Hero.jumlah)
