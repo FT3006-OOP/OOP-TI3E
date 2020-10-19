f1bd1660e27b73ab69569486ec9bfddf8489c383
bagussalam83 <salambagus.bs@gmail.com>
2020-10-18 22:16:54 -0700

    Memahami konsep Class dan instance attributes

---
+++ A2.1900025/#o4 - Class dan intance attributes/Main.py
@@ -0,0 +1,20 @@
+class Hero: # Template
+    #class variabel
+    jumlah = 0
+
+    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        # intance variabel
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
+        Hero.jumlah += 1
+        print ("membuat hero dengan nama" + inputName)
+
+
+hero1 = Hero("single",100, 10, 4)
+print(Hero.jumlah)
+hero2 = Hero("mirana",100, 15, 1)
+print(Hero.jumlah)
+hero3 = Hero("ucup",1000, 100, 0)
+print(Hero.jumlah)
