838998c09e509af5b8632ee54f4d633e73b254ec
Jimmi Andreansa <jimi.rgt17@gmail.com>
2020-10-21 13:40:46 +0700

    menambahkan class dan instance attributes

--- A2.1900093/Main.py
+++ A2.1900093/Main.py
@@ -1,16 +1,20 @@
-class Hero: # template
-
-    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
-        self.name = inputName
-        self.health = inputHealth
-        self.power = inputPower
-        self.armor = inputArmor
+class Hero: # Template
+    pass
 
 
-hero1 = Hero("sniper",100, 10, 4)
-hero2 = Hero("mirana",100, 15, 1)
-hero3 = Hero("ucup",1000, 100, 0)
+hero1 = Hero() # Object / instance (instanciate)
+hero2 = Hero()
+hero3 = Hero();
 
+hero1.name = "sniper"
+hero1.health = 100
+
+hero2.name = "sven"
+hero2.health = 200
+
+hero3.name = "ucup"
+hero3.health = 1000
+
+print(hero1)
 print(hero1.__dict__)
-print(hero2.__dict__)
-print(hero3.__dict__)
+print(hero1.name)
