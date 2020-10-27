46c361e3e5b6e2ef3afb3f18840b7be3020b6ca6
DeriFebriansyah <derryfebriansyah291@gmail.com>
2020-10-27 16:37:59 +0700

    Memahami Class dan instance attributes

--- A2.1900005/#04 - Class dan instance attributes/Main.py
+++ A2.1900005/#04 - Class dan instance attributes/Main.py
@@ -1,9 +1,9 @@
-class Hero: #template
-    #class variabel
+class Hero: # template
+    #class Variabel
     jumlah = 0
 
     def __init__(self, inputName, inputHealth, inputPower, inputArmor):
-        # instance variabel
+        # instance Variable
         self.name = inputName
         self.health = inputHealth
         self.power = inputPower
@@ -11,10 +11,9 @@ class Hero: #template
         Hero.jumlah += 1
         print("membuat Hero dengan nama " + inputName)
 
-
-hero1 = Hero("Sniper", 100, 10, 4)
+hero1 = Hero("sniper",100, 10, 4)
 print(Hero.jumlah)
-hero2 = Hero("Mirana", 100, 15, 1)
+hero2 = Hero("mirana",100, 15, 1)
 print(Hero.jumlah)
-hero3 = Hero("Ucup", 1000, 100, 0)
-print(Hero.jumlah)
+hero3 = Hero("ucup",1000, 100, 0)
+print(Hero.jumlah) 
