e014059a821351bfbff230eba3a0071fb18f7968
Fahmi hidayat A2.1900060 <fahmihidayat45472@gmail.com>
2020-11-06 22:31:33 +0700

    Menambahkan file Latihan OOP Games Basic

--- A2.1900060/#06 - Latihan OOP Games Basic/Main.py
+++ A2.1900060/#06 - Latihan OOP Games Basic/Main.py
@@ -1,31 +1,34 @@
-class   Hero:
+class Hero:
 
-    def __init__(self,name,healt,attackPower,armorNumber):
-        self.name = name
-        self.healt = healt
-        self.attackPower = attackPower
-        self.armorNumber = armorNumber
+ 	def __init__(self,name,health,attackPower,armorNumber):
+ 		self.name = name
+ 		self.health = health
+ 		self.attackPower = attackPower
+ 		self.armorNumber = armorNumber
 
-    def serang(self, lawan):
-        print(self.name + ' menyerang ' + lawan.name )
-        lawan.diserang(self, self.attackPower):
-    def diserang(self. lawan, attackPower_lawan):
-        print(self.name + ' diserang ' + lawan.name)
-        attack_diterima = attackPower_lawan/self.armorNumber
-        print('serangan terasa : ' str(attack_diterima))
-        self.healt -= attack_diterima
-        print('darah ' + self.name ' tersisa ' + str(self.healt))
+ 	def serang(self, lawan):
+ 		print(self.name + ' menyerang ' + lawan.name )
+ 		lawan.diserang(self, self.attackPower)
 
-        sniper = Hero('sniper',100,10,5)
-        rikimaru = Hero('rikimaru',100,20,10)
+ 	def diserang(self, lawan, attackPower_lawan):
+ 		print(self.name + ' diserang ' + lawan.name)
+ 		attack_diterima = attackPower_lawan/self.armorNumber
+ 		print('serangan terasa : ' + str(attack_diterima))
+ 		self.health -= attack_diterima
+ 		print('darah ' + self.name + ' tersisa ' + str(self.health))
 
-        sniper.serang(rikimaru)
-        print("\n")
-        rikimaru.serang(sniper)
-        print("\n")
-        rikimaru.serang(sniper)
-        print("\n")
-        rikimaru.serang(sniper)
-        print("\n")
-        rikimaru.serang(sniper)
+ sniper = Hero('sniper',100,10,5)
+ rikimaru = Hero('rikimaru',100,20,10)
+
+ sniper.serang(rikimaru)
+ print("\n")
+ rikimaru.serang(sniper)
+ print("\n")
+ rikimaru.serang(sniper)
+ print("\n")
+ rikimaru.serang(sniper)
+ print("\n")
+ rikimaru.serang(sniper)
+ print("\n")
+ rikimaru.serang(sniper)
         
