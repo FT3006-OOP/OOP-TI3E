b36000781221fe99bba9236c6417c54ea3b91e4a
A21900038-DedeSupriatnaa <A2.1900038@mhs.stmik-sumedang.ac.id>
2020-11-14 13:42:05 +0700

    UTS PBO

---
+++ A2.1900038/#8 - UTS/3. PentingnyaSpasidiPython.py
@@ -0,0 +1,10 @@
+#CONTOH
+#Komputer = 1 contoh
+#    laptop = 2 contoh
+
+#SETELAH DIUBAH SESUAI INTRUKSI
+Komputer = 1
+laptop = 2
+
+print ("Jumlah Komputer: ",(Komputer))
+print ("Jumlah Laptop: ",(laptop))
---
+++ A2.1900038/#8 - UTS/5. TentangClass.py
@@ -0,0 +1,7 @@
+#CONTOH
+#class Gadget(object):
+#  sumber_energi = "listrik"
+
+#SETELAH DIUBAH SESUAI INTRUKSI
+class Kendaraan():
+  sumber_energi = "Bensin"
