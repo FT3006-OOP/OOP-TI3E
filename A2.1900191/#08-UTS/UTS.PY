1530f78eded226b68cab224ae7883636ed98b1d9
zalfanurrobbyarkhan/ <a2.1900191@mhs.stmik-sumedang.ac.id>
2020-11-16 13:38:30 +0700

    uts

---
+++ A2.1900191/#08-UTS/UTS.PY
@@ -0,0 +1,39 @@
+#1
+laptop = 2
+
+#2
+# angka_saya telah kita beri nilai 1945
+
+angka_saya = 1945
+
+# setelah didefinisikan dan diberi nilai
+# kita bisa mengubah nilainya
+
+angka_saya = 1
+
+# Untuk melihat kalau kita telah mengubahnya,
+# kita akan mencetaknya dengan print
+
+print (angka_saya)
+
+#3
+Komputer = 1
+laptop = 2
+
+
+#4
+P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
+
+
+
+# variabel panjang nih
+contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
+
+
+#5&6
+class Kendaraan(object):
+    bahan_bakar = "bensin"
+
+mobil = Kendaraan()
+
+print (mobil.bahan_bakar)
