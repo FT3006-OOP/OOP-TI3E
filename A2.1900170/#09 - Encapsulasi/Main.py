5e03da31beda3b8ca0af5e6bd06ba630271b49fb
SilviFauziaAdzani <silvifauzia4901@gmail.com>
2020-11-21 18:04:01 +0700

    Menambahkan encapsulasi

---
+++ A2.1900170/#09 - Encapsulasi/Main.py
@@ -0,0 +1,28 @@
+class Hero:
+    def __init__(self,name,health,attackPower):
+        self.__name = name
+        self.__health = health
+        self.__attPower = attackPower
+    
+
+    def getName(self):
+        return self.__name
+    
+    def getHealth(self):
+        return self.__health
+    
+    
+    def diserang(self,serangPower):
+        self.__health -= serangPower
+    
+    def setAttpower(self,nilaibaru):
+        self.__attPower = nilaibaru
+
+
+earthshaker = Hero("eartshaker",50,5)
+
+
+print(earthshaker.getName())
+print(earthshaker.getHealth())
+earthshaker.diserang(5)
+print(earthshaker.getHealth())
