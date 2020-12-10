6212f8217197c5d6f1ff60fe73d950ba1f6594a4
SandyZaenalMustopa <a2.190064@mhs.stmik-sumedang.ac.id>
2020-12-10 20:33:48 +0700

    getter & setter_encapsulasi_pythonic_way

---
+++ A2.1900164/#11 - getter & setter_encapsulasi_pythonic_way/Main.py
@@ -0,0 +1,44 @@
+class Hero:
+
+    def __init__(self, name, health, armor):
+        self.name = name
+        self.__health = health
+        self.__armor = armor
+
+#self.info = "name {} : \n\health: {}".format(self.name.__health)        
+
+    @property
+    def info(self):
+        return "name {} : \n\health: {}".format(self.name.__health)
+
+    @property
+    def armor(self):
+        pass
+
+    @armor.getter
+    def armor(self):
+        return self.__armor
+
+    @armor.setter
+    def armor(self, input):
+
+    @armor.deleter
+    def armor(self):
+        print('armor di delet')
+        self.__armor = None 
+
+sniper = Hero('sniper',100,10)
+
+print('merubah info')
+print(sniper.info)
+sniper.name = 'dadang
+print(sniper.info)
+
+print('getter dan setter untuk __armor:')
+print(sniper.armor)
+sniper.armor = 50
+print(sniper.info)
+
+print('delete armor')
+del sniper.armor
+print(sniper.__dict__)
