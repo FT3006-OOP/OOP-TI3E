a1ca08cb02677e12dec831bc26746aa7bac4c7ea
RobyPurbaSakty <a2.1900158@mhs.stmik-sumedang.ac.id>
2020-12-14 19:29:52 +0700

    latihan encapulasi

---
+++ A2.1900158/#12 - Latihan Encapsulasi/main.py
@@ -0,0 +1,52 @@
+class Hero:
+
+    #provate clss variabel
+    __jumlah = 0
+    def __init__(self, name, health, attPower, armor):
+         self.__name = name
+         self.__healthStandar = health
+         self.__attPowerStandar = attPower
+         self.__armorStandar = armor
+         self.__level = 1
+         self.__exp = 0
+
+         self.__healthMax = self.__healthStandar * self.__level
+         self.__attPower = self.__attPowerStandar  *self.__level
+         self.__armor =  self.__armorStandar * self.__level
+
+         self.__health= self.__healthMax
+
+         Hero.__jumlah += 1
+
+    @property
+    def info(self):
+        return "{} level {}: \n\thealth = {}/{} \n\tattack = {} \n\tarmor = {}".format(self.__name,self.__level,self.__health,self.__healthMax,self.__attPower,self.__armor)
+
+    @property
+    def gainExp(self):
+        pass
+
+    @gainExp.setter
+    def gainExp (self, addexp):
+        self.__exp += addexp
+        if(self.__exp >= 100):
+            print(self.__name, 'level up')
+            self.__level += 1
+            self.__exp -= 100
+
+            self.__healthMax = self.__healthStandar * self.__level
+            self.__attPower = self.__attPowerStandar  *self.__level
+            self.__armor =  self.__armorStandar * self.__level        
+    def attack(self, musuh):
+        self.gainExp =50
+
+
+slarrdar = Hero ("slardar", 100, 5, 10)
+axe = Hero ("axe", 100, 5, 10)
+print(slarrdar.info)
+
+slarrdar.attack(axe)
+slarrdar.attack(axe)
+slarrdar.attack(axe)
+
+print(slarrdar.gainExp)
